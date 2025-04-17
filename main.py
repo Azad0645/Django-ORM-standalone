48f6943ae58f297ab0835e7eba97b4b4e2c2f071
Azad0645 <fenix16450@gmail.com>
2025-04-18 02:14:55 +0300

    Filter the database

--- main.py
+++ main.py
@@ -5,8 +5,11 @@ import django
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'settings')
 django.setup()
 
-from datacenter.models import Passcard  # noqa: E402
+from datacenter.models import Passcard
 
 if __name__ == '__main__':
-    # Программируем здесь
-    print('Количество пропусков:', Passcard.objects.count())  # noqa: T001
+    total_passcards = Passcard.objects.count()
+    active_passcards = Passcard.objects.filter(is_active=True).count()
+
+    print(f'Всего пропусков: {total_passcards}')
+    print(f'Активных пропусков: {active_passcards}')
